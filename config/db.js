595b695d82d4151d66b7b3a70da822abfa68f1e4
DemianBrinville <demian.brinville.p@gmail.com>
2026-07-08 19:14:07 -0300

    fadfas

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 
 /* --- CÓDIGO ORIGINAL ---
 const sequelize = new Sequelize("tasks_db", "root", "root", {
@@ -13,22 +14,16 @@ const sequelize = new Sequelize("tasks_db", "root", "root", {
 // EXPLICACIÓN: Evaluamos si existe la variable "DATABASE_URL" (que solo estará en producción).
 // Si existe, Sequelize se conecta a la base de datos PostgreSQL de Supabase aplicando SSL obligatorio.
 // Si no existe, el código automáticamente usará la configuración local de MySQL (retrocompatibilidad).
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-      logging: false,
-    })
-  : new Sequelize("tasks_db", "root", "root", {
-      host: "127.0.0.1",
-      port: 3306,
-      dialect: "mysql",
-      logging: false,
-    });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectModule: pg, // <-- 2. Obliga a Sequelize a usar este módulo inyectado
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Requerido para Supabase en producción
+    },
+  },
+});
 
 module.exports = sequelize;
